3bad5fbd3d423767abaf27de8da70fe627322626
yuma14 <yuma140902@gmail.com>
2024-03-02 21:44:43 +0900

    2021-04-28

---
+++ cpp/abc048/a.rs
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef int64_t ll;
+const int INF = 900000;
+const ll INFL = 9000000;
+
+#define rep(i, n) for(int i=0; i < (n); ++i)
+#define per(i, n) for(int i=(n)-1; i >= 0; --i)
+#define range(i, begin, end) for(int i=(begin); i < (end); ++i)
+
+#define ALL(vec) vec.begin(), vec.end()
+
+
+void solve(std::string AtCoder, std::string s, std::string Contest){
+	cout << "A" << s[0] << "C" << endl;
+}
+
+int main(){
+  std::string AtCoder;
+		std::cin >> AtCoder;
+		std::string s;
+		std::cin >> s;
+		std::string Contest;
+		std::cin >> Contest;
+  solve(AtCoder, s, Contest);
+  return 0;
+}
+
