1ea233d1a16f6cfe19e27ca51804d52be94253fc
yuma14 <yuma140902@gmail.com>
2024-03-02 21:27:28 +0900

    2020-06-21

---
+++ cpp/abc171/a.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef int64_t ll;
+typedef tuple<int, int> ii;
+typedef tuple<int, int, int> iii;
+const int INF = 900000;
+const ll INFL = 9000000;
+
+#define vi vector<int>
+#define vii vector<vector<int> >
+#define vi_def(n) vector<int>((n))
+#define vii_def(n,m) vector<vector<int> >((n), vector<int>((m)))
+#define vi_def_dflt(n, def) vector<int>((n), (def))
+#define vii_def_dflt(n, m, def) vector<vector<int> >((n), vector<int>((m), (def)))
+
+#define rep(i, n) for(int i=0; i < (n); ++i)
+#define per(i, n) for(int i=(n)-1; i >= 0; --i)
+#define range(i, begin, end) for(int i=(begin); i < (end); ++i)
+
+#define ALL(vec) vec.begin(), vec.end()
+
+#ifdef DBG_FLAG
+#define pdbg(msg) cout << (msg)
+#define pdbgl(msg) cout << (msg) << endl
+#define dbg if(true)
+#else
+#define pdbg(msg) ;
+#define pdbgl(msg) ;
+#define dbg if(false)
+#endif
+
+int main(){
+  char c;
+  cin >> c;
+  if('A' <= c && c <= 'Z') {
+    cout << "A" << endl;
+  }
+  else{
+    cout << "a" << endl;
+  }
+}
+
