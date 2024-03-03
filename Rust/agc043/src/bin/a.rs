c08128cd5e48c0169b20a8b276cc5f73e6fd7ac1
yuma14 <yuma140902@gmail.com>
2024-03-03 21:32:43 +0900

    agc043 a
    use vector

--- Rust/agc043/src/bin/a.rs
+++ Rust/agc043/src/bin/a.rs
@@ -1,16 +1,10 @@
-use im_rc::HashMap;
 use itertools::Itertools;
 
-fn solve_partial(
-    r: usize,
-    c: usize,
-    field: &Vec<Vec<char>>,
-    dp: &HashMap<(usize, usize), u64>,
-) -> u64 {
+fn solve_partial(r: usize, c: usize, field: &Vec<Vec<char>>, dp: &Vec<Vec<u64>>) -> u64 {
     let up = if r == 0 {
         u64::MAX
     } else {
-        dp[&(r - 1, c)]
+        dp[r - 1][c]
             + if field[r][c] == '#' && field[r - 1][c] == '#' {
                 0
             } else if field[r][c] == '#' {
@@ -22,7 +16,7 @@ fn solve_partial(
     let left = if c == 0 {
         u64::MAX
     } else {
-        dp[&(r, c - 1)]
+        dp[r][c - 1]
             + if field[r][c] == '#' && field[r][c - 1] == '#' {
                 0
             } else if field[r][c] == '#' {
@@ -38,31 +32,27 @@ fn solve_partial(
 #[argio::argio]
 fn main(h: usize, w: usize, s: [String; h]) -> u64 {
     let field = s.into_iter().map(|s| s.chars().collect_vec()).collect_vec();
-    let mut dp = HashMap::new();
+    let mut dp = vec![vec![u64::MAX; w]; h];
 
-    if field[0][0] == '#' {
-        dp.insert((0, 0), 1);
-    } else {
-        dp.insert((0, 0), 0);
-    }
+    dp[0][0] = if field[0][0] == '#' { 1 } else { 0 };
     for distance in 1..(h + w) {
         for r in 0..=distance {
             let c = distance - r;
             if r < h && c < w {
-                dp.insert((r, c), solve_partial(r, c, &field, &dp));
+                dp[r][c] = dp[r][c].min(solve_partial(r, c, &field, &dp));
             }
         }
     }
 
     /*
-        for r in 0..h {
-            for c in 0..w {
-                let v = dp[&(r, c)];
-                eprint!("{} ", v);
-            }
-            eprintln!();
+    for r in 0..h {
+        for c in 0..w {
+            let v = dp[r][c];
+            eprint!("{} ", v);
         }
+        eprintln!();
+    }
     */
 
-    dp[&(h - 1, w - 1)]
+    dp[h - 1][w - 1]
 }
