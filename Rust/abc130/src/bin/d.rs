ff79de74076fcd04716da64ba38352c9107970fa
yuma14 <yuma140902@gmail.com>
2024-03-06 00:14:00 +0900

    abc130 d (しゃくとり法)

--- Rust/abc130/src/bin/d.rs
+++ Rust/abc130/src/bin/d.rs
@@ -1,23 +1,31 @@
 #[allow(unused_imports)]
 use std::collections::*;
 
-use superslice::Ext;
-
 #[argio::argio]
 fn main(n: usize, k: u64, a: [u64; n]) -> usize {
-    let mut b = vec![0; n + 1];
-
-    b[0] = 0;
-    for i in 0..n {
-        b[i + 1] = b[i] + a[i];
-    }
-
-    dbg!(&b);
     let mut count = 0;
-    for i in 0..b.len() {
-        let c = b[i..].lower_bound(&(k + b[i]));
-        if c < b.len() - i {
-            count += b.len() - i - c;
+
+    let mut right = 0;
+    let mut sum = 0;
+    for left in 0..n {
+        /* [left, right)の総和がk以上になる最小のrightを求める */
+        while right < n && sum < k {
+            sum += a[right];
+            right += 1;
+        }
+
+        if sum < k {
+            /* [left, right)の総和がk以上になるrightは存在しない */
+            break;
+        }
+        count += n - (right - 1);
+
+        if left == right {
+            /* leftがrightに追いついたらrightも一緒に増やす */
+            right += 1;
+        } else {
+            /* leftがインクリメントされるのでsumを減らす */
+            sum -= a[left];
         }
     }
 
