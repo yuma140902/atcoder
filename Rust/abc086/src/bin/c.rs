21f2b8e86ca0828bf390e2ad9bee3408ecb55574
yuma14 <yuma140902@gmail.com>
2024-04-20 20:30:14 +0900

    abc086 c

--- Rust/abc086/src/bin/c.rs
+++ Rust/abc086/src/bin/c.rs
@@ -1,3 +1,21 @@
-fn main() {
-    todo!();
+use itertools::Itertools;
+
+#[argio::argio]
+fn main(n: usize, txy: [(i64, i64, i64); n]) -> String {
+    let txy = Some((0, 0, 0))
+        .into_iter()
+        .chain(txy.into_iter())
+        .collect_vec();
+
+    for i in 0..(txy.len() - 1) {
+        let (t1, x1, y1) = txy[i];
+        let (t2, x2, y2) = txy[i + 1];
+
+        let dt = t2 - t1;
+        let distance = (x2 - x1).abs() + (y2 - y1).abs();
+        if !(dt >= distance && (dt - distance) % 2 == 0) {
+            return "No".to_string();
+        }
+    }
+    return "Yes".to_string();
 }
