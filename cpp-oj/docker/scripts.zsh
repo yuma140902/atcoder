c75dafd137607037d10901e4e12a20267fc7445f
yuma14 <yuma140902@gmail.com>
2024-05-06 02:30:44 +0900

    update

--- cpp-oj/docker/scripts.zsh
+++ cpp-oj/docker/scripts.zsh
@@ -10,39 +10,9 @@ function ojd() {
 }
 
 funption ojt() {
-    local prog="$0"
-    local compiler="gcc"
-    local mode="debug"
-    while [[ $1 -gt 0 ]]; do
-        case $1 in
-            -h|--help)
-                echo "$prog [options]"
-                echo ""
-                echo "OPTIONS:"
-                echo "  -h, --help       show this message"
-                echo "  -c, --compiler   'gcc' or 'clang' [default=gcc]"
-                echo "  -d, --debug      debug mode [default]"
-                echo "  -o, --optimized  optimized mode"
-                exit 0
-                ;;
-            -c|--compiler)
-                local compiler="$2"
-                shift
-                shift
-                ;;
-            -d|--debug)
-                local mode=debug
-                shift
-                ;;
-            -o|--optimized)
-                local mode=optimized
-                shift
-                ;;
-            *)
-                echo "Unknown option $1"
-                exit 1
-                ;;
-        esac
-    done
-    make $mode-$compiler && oj t -c "./$mode-$compiler"
+    make debug-gcc && oj t -c "./debug-gcc"
+}
+
+funption ojto() {
+    make $1 && oj t -c "./$1
 }
